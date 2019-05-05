a2191892ed6aedfcccdb93e214eff19b7bcd0961
Matthew Whitworth <mattwhitworth5@gmail.com>
2019-05-05 00:37:20 -0400

    Added lots of mods

---
+++ oxide/plugins/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Oxide.Plugins
+{
+    [ Info( "RustExample", "ted.lua", "0.0.1" ) ]
+    public class ClassLibary3 : RustPlugin
+    {
+        void Init()
+        {
+            Puts("RustExample Plugin has loaded.");
+        }
+    }
+}
